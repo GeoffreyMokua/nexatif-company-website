ce2e17d4a3f790282a2f3a5329578370f6e37101
Geoffrey Mokua <arisimokua280@gmail.com>
2023-08-21 09:50:41 +0300

    added blog list

--- src/data/blog-list.js
+++ src/data/blog-list.js
@@ -1,9 +1,9 @@
-import blogImg1 from '../assets/images/blog/blog-img1.webp';
-import blogImg2 from '../assets/images/blog/blog-img2.webp';
-import blogImg3 from '../assets/images/blog/blog-img3.webp';
-import blogImg4 from '../assets/images/blog/blog-img4.webp';
-import blogImg5 from '../assets/images/blog/blog-img5.webp';
-import blogImg6 from '../assets/images/blog/blog-img6.webp';
+import blogImg1 from '../assets/images/blog/blog1.jpg';
+import blogImg2 from '../assets/images/blog/blog2.jpg';
+import blogImg3 from '../assets/images/blog/blog3.jpg';
+import blogImg4 from '../assets/images/blog/blog4.jpg';
+import blogImg5 from '../assets/images/blog/blog5.jpg';
+import blogImg6 from '../assets/images/blog/blog6.jpg';
 
 const BlogList = [
   {
@@ -12,7 +12,7 @@ const BlogList = [
     imgSrc: blogImg1,
     title: 'From a small startup to a leading global agency in 10 Years.',
     category: 'Strategy',
-    date: 'Jan 15, 2023',
+    date: 'May 15, 2023',
     description:
       'Imagine that a few more steps ahead there is a place where the path splits, where is goes off to the left and off to the right. Pause here for a few moments and have a think. Here, there are two different pathways, two possibilities, two ways that you could choose to go.',
     mainDescription:
@@ -23,7 +23,7 @@ const BlogList = [
     imgSrc: blogImg2,
     title: "How google's BERT algorithm affects your website traffic",
     category: 'Jobs',
-    date: 'Jan 30, 2023',
+    date: 'June 30, 2023',
     description:
       "The first thing to remember about success is that it is a process - nothing more, nothing less. There is really no magic to it and it's not reserved only for a select few people. It really comes down to understanding the steps in the process and then executing on those steps.",
     mainDescription:
@@ -34,7 +34,7 @@ const BlogList = [
     imgSrc: blogImg3,
     title: 'Five reasons why you must create a website for your company',
     category: 'Technology',
-    date: 'Feb 15, 2023',
+    date: 'March 15, 2023',
     description:
       'There are basically six key areas to higher achievement. Some people will tell you there are four while others may tell you there are eight. One thing for certain though, is that irrespective of the number of steps the experts talk about, they all originate from the same roots.',
     mainDescription:
@@ -46,7 +46,7 @@ const BlogList = [
     title:
       'Corporations that Prove Data is Key to Victorious Digital Transformation',
     category: 'Marketing',
-    date: 'Feb 28, 2023',
+    date: 'April 28, 2023',
     description:
       'Making a decision to do something - this is the first step. We all know that nothing moves until someone makes a decision. The first action is always in making the decision to proceed. This is a fundamental step, which most people overlook.',
     mainDescription:
@@ -57,7 +57,7 @@ const BlogList = [
     imgSrc: blogImg5,
     title: 'Google, Facebook or Instagram Which one is best for my business?',
     category: 'Finance',
-    date: 'Mar 15, 2023',
+    date: 'Mar 26, 2023',
     description:
       "There is really no magic to it and it's not reserved only for a select few people. As such, success really has nothing to do with luck, coincidence or fate. It really comes down to understanding the steps in the process and then executing on those steps.",
     mainDescription:
